Fix various trash_list issues and energy calculation race condition

ManifoldEM/CC/ComputePsiMovieEdgeMeasurements.py
@@ -354,7 +354,7 @@ def checkBadPsis(trash_list, tau_occ_thresh=0.35):
 
     if nodesAllBadPsis.shape[0] > 0:
         print('nodesAllBadPsis', nodesAllBadPsis)
-        trash_list[nodesAllBadPsis] = int(1)
+        trash_list[nodesAllBadPsis] = True
 
     return trash_list, num_nodesAllBadPsis
 
@@ -375,7 +375,7 @@ def op(G, nodeRange, edgeNumRange, *argv):
             ComputeOpticalFlowPrDAll.op(nodeEdgeNumRange)
 
     ## check for bad PDs found based on bad tau values
-    trash_list = np.array(p.trash_list)
+    trash_list = np.array(p.trash_list, dtype=bool)
     p.tau_occ_thresh = 0.35  # interface with GUI, p.py
     tau_occ_thresh = p.tau_occ_thresh
     # take the already existing trash_list and update it

ManifoldEM/ComputeEnergy1D.py
@@ -27,10 +27,10 @@ def op(*argv):
     #print psiNumsAll.shape,'a=',a
     xSelect = np.delete(xSelect, a)
     a = np.nonzero(p.trash_list == 1)[0]  # unassigned states, python
-    if xSelect:
+    if len(xSelect):
         xSelect = np.delete(xSelect, a)
 
-    if not xSelect:
+    if not len(xSelect):
         debug_print("No assigned states: Unable to compute energy profile")
 
 
@@ -77,10 +77,6 @@ def op(*argv):
                [trajTaus, posPsi1All, posPathAll, xSelect, tauAvg])
     gc.collect()
 
-    if argv:
-        progress7 = argv[0]
-        progress7.emit(100)
-
     p.hUn = hUn
     OM_file = '{}OM'.format(p.OM_file)
     hUn.astype('int').tofile(OM_file)
@@ -97,13 +93,19 @@ def op(*argv):
     E.astype('float').tofile(OM1_file)
 
     set_params.op(0)
+
+    if argv:
+        progress7 = argv[0]
+        progress7.emit(100)
+
     return hUn
 
 
 if __name__ == '__main__':
-    p.user_dir = '../'
-    p.out_dir = os.path.join(p.user_dir, 'data_output/')
-    p.tess_file = '{}/selecGCs'.format(p.out_dir)
+    import sys
+    if len(sys.argv) != 2:
+        sys.exit(1)
 
+    p.load(sys.argv[1])
     p.create_dir()
     op()

ManifoldEM/FindCCGraphPruned.py
@@ -247,9 +247,9 @@ def CalcPairwiseDistS2(X, *argv):
 
 
 def op(CC_graph_file_pruned):
-    trash_list = np.array(p.trash_list)
-    good_nodes = np.nonzero(trash_list == 0)[0]
-    bad_nodes = np.nonzero(trash_list == 1)[0]
+    trash_list = np.array(p.trash_list, dtype=bool)
+    good_nodes = np.nonzero(~trash_list)[0]
+    bad_nodes = np.nonzero(trash_list)[0]
     numNodes = len(good_nodes)
     num_pruned_nodes = len(bad_nodes)
 