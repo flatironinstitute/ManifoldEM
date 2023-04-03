52296ed6d9587720888e0f369d040bdcc68e9711
Robert Blackwell <rblackwell@flatironinstitute.org>
2023-04-03 15:19:54 -0400

    More cruft removal

--- ManifoldEM/CC/ComputeMeasureEdgeAll.py
+++ ManifoldEM/CC/ComputeMeasureEdgeAll.py
@@ -9,14 +9,10 @@ import numpy as np
 from numpy import linalg as LA
 from functools import partial
 from contextlib import contextmanager
-from subprocess import Popen
 
 from ManifoldEM import p, myio, set_params
 from ManifoldEM.CC.OpticalFlowMovie import SelectFlowVec
 from ManifoldEM.CC.hogHistogram import histogram_from_gradients, visualise_histogram
-
-_logger = logging.getLogger(__name__)
-_logger.setLevel(logging.DEBUG)
 
 
 @contextmanager
@@ -36,7 +32,6 @@ def poolcontext(*args, **kwargs):
 
 
 def HOGOpticalFlowPy(flowVec, hogFigfile):
-
     cellSize = (4, 4)  # this is actually the block size
     cellsPerBlock = (2, 2)
     visualize = False
@@ -56,7 +51,6 @@ def HOGOpticalFlowPy(flowVec, hogFigfile):
                       same_size=sameSize)
 
     VxDim = flowVec['Vx'].shape
-    #print 'VxDim',VxDim
     if len(VxDim) > 2:
         VxStackDim = VxDim[2]
 
@@ -96,53 +90,14 @@ def HOGOpticalFlowPy(flowVec, hogFigfile):
                                      flatten=flatten,
                                      same_size=sameSize)
 
-    ## this is very slow it print figure,only for testing purpose
-    showHOG = p.opt_movie['OFvisual']
-    printHOG = p.opt_movie['printFig']
-    if showHOG or printHOG:
-        dims = np.shape(H)
-        if len(dims) > 3:
-            nhogFigs = dims[3]
-        else:
-            nhogFigs = 1
-
-        for nh in range(0, nhogFigs):
-            if nhogFigs > 1:
-                hogFigfile_figs = hogFigfile + '_block' + str(nh)
-                Hblock = H[:, :, :, nh]
-            else:
-                hogFigfile_figs = hogFigfile
-                Hblock = H
-            fig = plt.figure('HOGFeature', figsize=(10, 10))
-            fig.clf()
-            imh2 = visualise_histogram(Hblock, cellSize[0], cellSize[1], signedOrientation)
-            plt.title('HOG features')
-            plt.imshow(imh2, cmap=plt.cm.Greys_r)
-            if showHOG:
-                plt.show()
-            if printHOG:
-                fig.savefig(hogFigfile_figs + '.png')
-
     return H, hog_params
 
 
-# plot
-def figurePlot(mat):
-    fig = plt.figure()
-    plt.imshow(mat, cmap=plt.cm.hot)
-    plt.show()
-
-
 # Compare how similar two Matrices/Images are.
-# TO DO: Implement error checking for wrong or, improper inputs
+# TODO: Implement error checking for wrong or, improper inputs
 # Check for NaN or Inf outputs , etc.
 def CompareOrientMatrix(FlowVecSelA, FlowVecSelB, prds_psinums, labels):
-
-    #FlowVxDim = FlowVecSelA['Vx'].shape
-    #FlowVxDim = FlowVecSelB['Vx'].shape
-    #print 'FlowVxDim',FlowVxDim
     useNorm = 'l2'
-    #if len(FlowVxDim) = 3, then HOGF will be 4-dimensional
     prD_A = prds_psinums[0]
     psinum_A = prds_psinums[1]
     prD_B = prds_psinums[2]
@@ -165,22 +120,15 @@ def CompareOrientMatrix(FlowVecSelA, FlowVecSelB, prds_psinums, labels):
     # if for some reason the number of blocks for movie A and B are different, then this check is a fail safe to make
     # the code still work
     hogDimA = HOGFA.shape
-    hogDimB = HOGFB.shape
-    #print 'hog shape A,B',HOGFA.shape,HOGFB.shape
 
-    #print 'hog shape after adjustment',HOGFA.shape,HOGFB.shape
     hoffset = 1.25
     distHOGAB = []
     distHOGAB_tblock = []
     isBadPsiAB_block = []
     hp = np.ceil(np.float(hogDimA[0]) / hog_params['cell_size'][0]).astype(int)
-    #print 'hp',hp
     num_hogel_th = np.ceil(0.2 * (hp**2) * hogDimA[2]).astype(int)
-    #print 'num_hogel_th',num_hogel_th
 
     if useNorm == 'l1':
-        #L1-norm
-        #time block
         if len(hogDimA) > 3:
 
             distHOGAB_tblock = np.zeros((hogDimA[3], 1))
@@ -188,15 +136,11 @@ def CompareOrientMatrix(FlowVecSelA, FlowVecSelB, prds_psinums, labels):
             isBadPsiB_block = np.zeros((hogDimA[3], 1))
 
             for j in range(0, hogDimA[3]):
-                #if  np.count_nonzero(HOGFA[:,:,:,j])==0:
                 if np.count_nonzero(HOGFA[:, :, :, j]) <= num_hogel_th:
-                    #print 'HOGFA',j,HOGFA[:,:,:,j]
                     HOGFA[:, :, :, j] = np.random.random(np.shape(HOGFB[:, :, :, j])) + hoffset
                     isBadPsiA_block[j] = 1
 
-                #if  np.count_nonzero(HOGFB[:,:,:,j])==0:
                 if np.count_nonzero(HOGFB[:, :, :, j]) <= num_hogel_th:
-                    #print 'HOGFB',j,HOGFB[:,:,:,j]
                     HOGFB[:, :, :, j] = np.random.random(np.shape(HOGFB[:, :, :, j])) + hoffset
                     isBadPsiB_block[j] = 1
 
@@ -208,9 +152,6 @@ def CompareOrientMatrix(FlowVecSelA, FlowVecSelB, prds_psinums, labels):
         distHOGAB = sum(abs(HOGFA - HOGFB))
 
     if useNorm == 'l2':
-        #L2-norm
-
-        #time block
         if len(hogDimA) > 3:
             distHOGAB_tblock = np.zeros((hogDimA[3], 1))
             isBadPsiA_block = np.zeros((hogDimA[3], 1))
@@ -220,17 +161,13 @@ def CompareOrientMatrix(FlowVecSelA, FlowVecSelB, prds_psinums, labels):
                 # hog feature matrix difference for A,B HOGFA - HOGFB will be smaller if either of the two matrices are
                 # all zeros, so to produce a maximum difference between a normal feature matrix and such zero
                 # feature matrix we can add some random numbers with a high value
-                #if  np.count_nonzero(HOGFA[:,:,:,j])==0: # have to check this criteria
-                #print 'nonZero_hogA_elements:', np.count_nonzero(HOGFA[:,:,:,j])
                 if np.count_nonzero(HOGFA[:, :, :, j]) <= num_hogel_th:  # have to check this criteria
-                    #print 'HOGFA',j,HOGFA[:,:,:,j]
                     HOGFA[:, :, :, j] = np.random.random(np.shape(HOGFB[:, :, :, j])) + hoffset
                     isBadPsiA_block[j] = 1
 
                 #if  np.count_nonzero(HOGFB[:,:,:,j])==0:
                 #print 'nonZero_hogB_elements:',np.count_nonzero(HOGFB[:,:,:,j])
                 if np.count_nonzero(HOGFB[:, :, :, j]) <= num_hogel_th:
-                    #print 'HOGFB',j,HOGFB[:,:,:,j]
                     HOGFB[:, :, :, j] = np.random.random(np.shape(HOGFB[:, :, :, j])) + hoffset
                     isBadPsiB_block[j] = 1
 
@@ -238,11 +175,9 @@ def CompareOrientMatrix(FlowVecSelA, FlowVecSelB, prds_psinums, labels):
 
             isBadPsiAB_block = [isBadPsiA_block.T, isBadPsiB_block.T]
 
-        #print 'isBadPsiAB_block',isBadPsiAB_block
         # this should be done after the adjustments of the zero matrix to a matrix with high random numbers
         distHOGAB = LA.norm(HOGFA - HOGFB)
 
-    #print 'distHOGAB_tblock',distHOGAB_tblock,
     varargout = [distHOGAB, distHOGAB_tblock, isBadPsiAB_block]
     return varargout
 
@@ -270,8 +205,6 @@ def ComputeMeasuresPsiMoviesOpticalFlow(FlowVecSelAFWD, FlowVecSelBFWD, FlowVecS
     psiMovMREV = psiMovOFMeasuresREV['Values']
     psiMovMREV_tblock = psiMovOFMeasuresREV['Values_tblock']
 
-    #print 'prds_psinums',prds_psinums,'isBadPsiAB_blockF',isBadPsiAB_blockF,'isBadPsiAB_blockR',isBadPsiAB_blockR
-
     psiMovieOFmeasures = dict(MeasABFWD=psiMovMFWD,
                               MeasABFWD_tblock=psiMovMFWD_tblock,
                               MeasABREV=psiMovMREV,
@@ -290,9 +223,7 @@ def ComputeEdgeMeasurePairWisePsiAll(input_data, G, flowVecPctThresh):
     nbrPrDPsiFile = '{}{}'.format(p.CC_OF_file, nbrPrD)
 
     NumPsis = p.num_psis
-    #print 'NumPsis',NumPsis
-    #load the data for the current and neighbor prds
-    #print '\nLoading data for current and neighbor PrD, Edge:{} ...'.format(edgeNum)
+    # load the data for the current and neighbor prds
     data = myio.fin1(currentPrDPsiFile)
     FlowVecCurrPrD = data['FlowVecPrD']
     data = myio.fin1(nbrPrDPsiFile)
@@ -309,7 +240,6 @@ def ComputeEdgeMeasurePairWisePsiAll(input_data, G, flowVecPctThresh):
     measureOFCurrNbrREV = np.empty((nEdges, NumPsis, NumPsis))
 
     if numtblocks > 1:
-        #numtblocks = 3 # temp , have to fix this from beginning
         measureOFCurrNbrFWD_tblock = np.empty((nEdges, NumPsis, NumPsis * numtblocks))
         measureOFCurrNbrREV_tblock = np.empty((nEdges, NumPsis, NumPsis * numtblocks))
 
@@ -318,15 +248,12 @@ def ComputeEdgeMeasurePairWisePsiAll(input_data, G, flowVecPctThresh):
 
     badNodesPsisBlock = np.zeros((G['nNodes'], NumPsis))
 
-    #loop over for psi selections for current prD
     for psinum_currPrD in psiSelcurrPrD:
-
         if FlowVecCurrPrD[psinum_currPrD]['FWD']:  # check if this condition holds for all kind of entries of the dict
             FlowVecCurrPrDFWD = SelectFlowVec(FlowVecCurrPrD[psinum_currPrD]['FWD'], flowVecPctThresh)
 
         # psi selection candidates for the neighboring prD
         for psinum_nbrPrD in psiCandidatesNnbrPrD:
-            #print('\nCurrent-PrD:{}, Current-PrD-Psi:{}\nNeighbor-PrD:{}, Neighbor-PrD-Psi:{}'.format(currPrD, psinum_currPrD, nbrPrD, psinum_nbrPrD))
             if FlowVecNbrPrD[psinum_nbrPrD]['REV']:
                 FlowVecNbrPrDFWD = SelectFlowVec(FlowVecNbrPrD[psinum_nbrPrD]['FWD'], flowVecPctThresh)
                 FlowVecNbrPrDREV = SelectFlowVec(FlowVecNbrPrD[psinum_nbrPrD]['REV'], flowVecPctThresh)
@@ -339,7 +266,7 @@ def ComputeEdgeMeasurePairWisePsiAll(input_data, G, flowVecPctThresh):
 
             psiMovieOFmeasures, isBadPsiAB_block = ComputeMeasuresPsiMoviesOpticalFlow(
                 FlowVecSelAFWD, FlowVecSelBFWD, FlowVecSelBREV, prds_psinums)
-            #print('psiMovieOFmeasures:',psiMovieOFmeasures,'\n')
+
             measureOFCurrNbrFWD[edgeNum][psinum_currPrD, psinum_nbrPrD] = psiMovieOFmeasures['MeasABFWD']
             measureOFCurrNbrREV[edgeNum][psinum_currPrD, psinum_nbrPrD] = psiMovieOFmeasures['MeasABREV']
 
@@ -349,7 +276,6 @@ def ComputeEdgeMeasurePairWisePsiAll(input_data, G, flowVecPctThresh):
 
             if numtblocks > 1:
                 t = psinum_nbrPrD * numtblocks
-                #time block
                 print('t', t, 'numtblocks', numtblocks)
                 measureOFCurrNbrFWD_tblock[edgeNum][psinum_currPrD, t:t + numtblocks] = np.transpose(
                     psiMovieOFmeasures['MeasABFWD_tblock'])
@@ -364,12 +290,6 @@ def ComputeEdgeMeasurePairWisePsiAll(input_data, G, flowVecPctThresh):
     else:
         measureOFCurrNbrEdge_tblock = []
 
-    #print '\nCurrent-PrD:{}, Neighbor-PrD:{}'.format(currPrD, nbrPrD)
-    #print 'measureOFCurrNbrEdge',measureOFCurrNbrEdge
-    #print 'measureOFCurrNbrEdge_tblock',measureOFCurrNbrEdge_tblock
-
-    #return
-    #print 'Writing edge-{} data to file'.format(edgeNum)
     myio.fout1(CC_meas_file, ['measureOFCurrNbrEdge', 'measureOFCurrNbrEdge_tblock', 'badNodesPsisBlock'],
                [measureOFCurrNbrEdge, measureOFCurrNbrEdge_tblock, badNodesPsisBlock])
 
@@ -386,7 +306,7 @@ def ComputeEdgeMeasurePairWisePsiAll(input_data, G, flowVecPctThresh):
 def divide1(N, G):
     ll = []
 
-    fin_edges = []  #collect list of previously finished PDs from CC/CC_meas/
+    fin_edges = []  # collect list of previously finished PDs from CC/CC_meas/
     for root, dirs, files in os.walk(p.CC_meas_prog):
         for file in sorted(files):
             if not file.startswith('.'):  #ignore hidden files
@@ -403,20 +323,6 @@ def divide1(N, G):
             ll.append([currPrD, nbrPrD, CC_meas_file, e])
 
     return ll
-
-
-def count1(R, G):
-    c = 0
-    for e in R:
-        currPrD = G['Edges'][e, 0]
-        nbrPrD = G['Edges'][e, 1]
-        CC_meas_file = '{}{}_{}_{}'.format(p.CC_meas_file, e, currPrD, nbrPrD)
-        if os.path.exists(CC_meas_file):
-            data = myio.fin1(CC_meas_file)
-            if data is not None:
-                continue
-        c += 1
-    return c
 
 
 def op(G, nodeEdgeNumRange, *argv):
