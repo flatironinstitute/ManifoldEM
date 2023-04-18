595b7db5da6e811cb8d2ab3b5eb99139df216ee9
Robert Blackwell <rblackwell@flatironinstitute.org>
2023-04-18 11:09:58 -0400

    cleanup: move some variables around for init in psiAnalysis

--- ManifoldEM/psiAnalysis.py
+++ ManifoldEM/psiAnalysis.py
@@ -15,21 +15,19 @@ Copyright (c) Evan Seitz 2019 (python version)
 '''
 
 
-def _construct_input_data(N, rc, fin_PDs):
+def _construct_input_data(N):
     ll = []
+    psi_nums_all = np.tile(np.array(range(p.num_psis)), (N, 1))  # numberofJobs x num_psis
+    senses_all = np.tile(np.ones(p.num_psis), (N, 1))  # numberofJobs x num_psis
+
     for prD in range(N):
         dist_file = p.get_dist_file(prD)
         psi_file = p.get_psi_file(prD)
         psi2_file = p.get_psi2_file(prD)
         EL_file = p.get_EL_file(prD)
-        psinums = rc['psiNumsAll'][prD, :]
-        senses = rc['sensesAll'][prD, :]
-        psi_list = []  # list of incomplete psi values per PD
-        for psi in range(len(psinums)):
-            if fin_PDs[int(prD), int(psi)] == int(1):
-                continue
-            psi_list.append(psi)
-
+        psinums = psi_nums_all[prD, :]
+        senses = senses_all[prD, :]
+        psi_list = list(range(len(psinums)))  # list of incomplete psi values per PD
         ll.append([dist_file, psi_file, psi2_file, EL_file, psinums, senses, prD, psi_list])
     return ll
 
@@ -40,12 +38,7 @@ def op(*argv):
     multiprocessing.set_start_method('fork', force=True)
     use_gui_progress = len(argv) > 0
 
-    psi_nums_all = np.tile(np.array(range(p.num_psis)), (p.numberofJobs, 1))  # numberofJobs x num_psis
-    senses_all = np.tile(np.ones(p.num_psis), (p.numberofJobs, 1))  # numberofJobs x num_psis
-    rc = {'psiNumsAll': psi_nums_all, 'sensesAll': senses_all}
-
-    fin_PDs = np.zeros(shape=(p.numberofJobs, p.num_psis), dtype=int)
-    input_data = _construct_input_data(p.numberofJobs, rc, fin_PDs)
+    input_data = _construct_input_data(p.numberofJobs)
     n_jobs = len(input_data)
     progress3 = argv[0] if use_gui_progress else NullEmitter()
     local_psi_func = partial(psiAnalysisParS2.op,
