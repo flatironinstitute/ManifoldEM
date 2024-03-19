3dc8f2f257159b94b1713bc307caaadca8c50de4
Robert Blackwell <rblackwell@flatironinstitute.org>
2024-03-19 10:09:09 -0400

    params: switch to type annotations to describe variables
    This will be helpful when providing a parameter viewing/editing interface

--- ManifoldEM/params.py
+++ ManifoldEM/params.py
@@ -9,13 +9,13 @@ Copyright (c) Columbia University Hstau Liao 2019
 Copyright (c) Columbia University Suvrajit Maji 2019
 Copyright (c) Flatiron Institute Robert Blackwell 2023
 """
+import numpy as np
 import os
+from pprint import pprint
 import toml
 import traceback
+from typing import Annotated
 
-import numpy as np
-
-from pprint import pprint
 
 # resProj structure:
 #     0: default; new project
@@ -30,72 +30,69 @@ class Params():
             cls.instance = super(Params, cls).__new__(cls)
         return cls.instance
 
-    proj_name: str = ''            # name of the project :D
-    resProj: int = 0               # see above
-    relion_data: bool = True       # working with relion data?
-    ncpu: int = 1                  # number of CPUs for multiprocessing
-    avg_vol_file: str = ''         # average volume file (e.g., .mrc)
-    img_stack_file: str = ''       # image stack file (e.g., .mrcs)
-    align_param_file: str = ''     # alignment file (e.g., .star)
-    mask_vol_file: str = ''        # mask volume file (e.g., .mrc)
-    num_part: int = 0              # total number of particles in stack
-    Cs: float = 0.0                # Spherical Aberration [mm] (from alignment file)
-    EkV: float = 0.0               # Voltage [kV] (from alignment file)
-    AmpContrast: float = 0.0       # Amplitude Contrast [ratio](from alignment file);
-    eps: float = 1E-10             # small fraction to be added if divide-by-zero errors occur
-    gaussEnv: float = float("inf") # envelope of CTF
+    proj_name: Annotated[str, 'Name of the project'] = ''
+    resProj: Annotated[int, 'Current analysis level of project']  = 0
+    relion_data: Annotated[bool, 'Is the code analyzing relion data?'] = True
+    ncpu: Annotated[int, 'Number of processes to use for multiprocessing'] = 1
+    avg_vol_file: Annotated[str, 'Average volume file (e.g., .mrc)'] = ''
+    img_stack_file: Annotated[str, 'Image stack file (e.g., .mrcs)'] = ''
+    align_param_file: Annotated[str, 'Alignment file (e.g., .star)'] = ''
+    mask_vol_file: Annotated[str, 'Mask volume file (e.g., .mrc)'] = ''
+    num_part: Annotated[int, 'Total number of particles in stack'] = 0
+    Cs: Annotated[float, 'Spherical Aberration [mm] (from alignment file)'] = 0.0
+    EkV: Annotated[float, 'Voltage [kV] (from alignment file)'] = 0.0
+    AmpContrast: Annotated[float, 'Amplitude Contrast [ratio](from alignment file)'] = 0.0
+    eps: Annotated[float, 'Small fraction to be added if divide-by-zero errors occur'] = 1E-10
+    gaussEnv: Annotated[float, 'envelope of CTF'] = float("inf")
 
     # microscopy parameters:
-    nPix: int = 0          # window size of image (e.g., for 100x100 image, nPix=100)
-    pix_size: float = 0.0  # pixel size of image [Angstroms] (known via rln_DetectorPixelSize*10e6 / rln_Magnification)
-    obj_diam: float = 0.0  # diameter of macromolecule [Angstroms]
-    resol_est: float = 0.0 # estimated resolution [Angstroms]
-    ap_index: int = 1      # aperture index {1,2,3...}; increases tessellated bin size
+    nPix: Annotated[int, 'Window size of image (e.g., for 100x100 image, nPix=100)'] = 0
+    pix_size: Annotated[float, 'Pixel size of image [Angstroms] (known via rln_DetectorPixelSize*10e6 / rln_Magnification)'] = 0.0
+    obj_diam: Annotated[float, 'Diameter of macromolecule [Angstroms]'] = 0.0
+    resol_est: Annotated[float, 'Estimated resolution [Angstroms]'] = 0.0
+    ap_index: Annotated[int, 'Aperture index {1,2,3...}; increases tessellated bin size'] = 1
 
     # tessellation binning:
-    PDsizeThL: int = 100   # minimum required snapshots in a tessellation for it be admitted
-    PDsizeThH: int = 2000  # maximum number of snapshots that will be considered within each tessellation
-    numberofJobs: int = 0  # total number of bins to consider for manifold embedding
-
+    PDsizeThL: Annotated[int, 'Minimum required snapshots in a tessellation for it be admitted'] = 100
+    PDsizeThH: Annotated[int, 'Maximum number of snapshots that will be considered within each tessellation'] = 2000
+    numberofJobs: Annotated[int, 'Total number of bins to consider for manifold embedding'] = 0
 
     # eigenfunction parameters:
-    num_eigs: int = 15      # number of highest-eigenvalue eigenfunctions to consider in total (max entry of eigenvalue spectrum)
-    num_psiTrunc: int = 8    # number of eigenfunctions for truncated views
-    num_psis: int = 8
+    num_eigs: Annotated[int, 'Number of highest-eigenvalue eigenfunctions to consider in total (max entry of eigenvalue spectrum)'] = 15
+    num_psiTrunc: Annotated[int, 'Number of eigenfunctions for truncated views'] = 8
+    num_psis: Annotated[int, 'Number of eigenfunctions'] = 8
+    rad: Annotated[int, 'Manifold pruning'] = 5
+
+    # NLSA parameters:
+    fps: Annotated[float, 'Frames per second for generated movies'] = 5.0
+    conOrderRange: Annotated[int, 'Coarse-graining factor of energy landscape'] = 50
     # tune automation suggestion (Ali): larger tune = smaller gaussian width; turns data into
     # islands/chunks (can't see long-range patterns); a good tune should form a 'good' psirec parabola.
     # as well, you can keep using tune to get rid of outliers in data; you want the number of outliers
     # to be around 10%; if higher than this, tune needs to be changed.
-    tune: int = 3           # diffusion map tuning; this needs to be automated
-    rad: int = 5            # manifold pruning
-    conOrderRange: int = 50 # coarse-graining factor of energy landscape
-
-    # NLSA movie parameters:
-    fps: float = 5.0
+    tune: Annotated[int, 'Diffusion map tuning parameter'] = 3
 
     # energy landscape parameters:
-    dim: int = 1              # user-defined number of dimensions (reaction coordinates); {1,2}
-    temperature: int = 25     # user-defined pre-quenching temperature of experiments
-    trajName: str = '1'       # filename for exported (2D) trajectories
-    nClass: int = 50          # number of states partitioned within each 1D reaction coordinate; results in a 50x1 1D ELS
-    width_1D: int = 1         # user-defined width of trajectory in 1D energy path
-    width_2D: int = 1         # user-defined width of trajectory in 2D energy path
-    hUh = None                # occupancy map
-
+    dim: Annotated[int, 'Number of reaction coordinates for energy landscape'] = 1
+    temperature: Annotated[int, 'User-defined pre-quenching temperature of experiments'] = 25
+    trajName: Annotated[str, 'Filename modifier for exported (2D) trajectories'] = '1'
+    nClass: Annotated[int, 'Number of states partitioned within each 1D reaction coordinate; results in a nClassx1 1D ELS'] = 50
+    width_1D: Annotated[int, 'Width of trajectory in 1D energy path'] = 1
+    width_2D: Annotated[int, 'Width of trajectory in 2D energy path'] = 1
 
     #  reaction coordinates parameters:
-    getOpticalFlow: bool = True                # default True to compute optical flow vectors
-    getAllEdgeMeasures: bool = True            # default True to compute edge measures
+    getOpticalFlow: Annotated[bool, 'Compute optical flow vectors'] = True
+    getAllEdgeMeasures: Annotated[bool, 'Compute "edge measures"'] = True
     opt_movie: dict = {'printFig': 0,
                        'OFvisual': 0,
                        'visual_CC': 0,
                        'flowVecPctThresh': 95}
-    opt_mask_type: int = 0                     # 0:None, 1:Annular, 2:Volumetric
-    opt_mask_param: int = 0                    # for either none, radius (Int), or iso(Int)
+    opt_mask_type: Annotated[int, '0:None, 1:Annular, 2:Volumetric'] = 0
+    opt_mask_param: Annotated[int, 'for either none, radius (Int), or iso(Int)'] = 0
 
-    findBadPsiTau: bool = True
-    tau_occ_thresh: float = 0.35
-    use_pruned_graph: bool = False
+    findBadPsiTau: Annotated[bool, ''] = True
+    tau_occ_thresh: Annotated[float, ''] = 0.35
+    use_pruned_graph: Annotated[bool, ''] = False
 
     visualization_params: dict = {
         'S2_scale': 1.0,
@@ -283,10 +280,11 @@ class Params():
         return f'{self.dist_file}prD_{prd_index}'
 
 
-    def todict(self):
+    def asdict(self):
         res = {}
         for var in dir(Params):
-            if not var.startswith('_') and isinstance(getattr(Params, var), (int, list, dict, float, str)):
+            objtype = getattr(Params, var)
+            if not var.startswith('_') and isinstance(objtype, (int, list, dict, float, str)):
                 res[var] = getattr(self, var)
 
         return res
@@ -295,7 +293,7 @@ class Params():
     def save(self, outfile: str = ''):
         if outfile == '':
             outfile = f'params_{self.proj_name}.toml'
-        res = {'params': self.todict()}
+        res = {'params': self.asdict()}
         with open(outfile, 'w') as f:
             toml.dump(res, f)
             f.flush()
@@ -321,7 +319,7 @@ class Params():
 
 
     def print(self):
-        pprint(self.todict())
+        pprint(self.asdict())
 
 
     def create_dir(self):
