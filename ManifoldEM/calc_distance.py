ca7ea1fafae90b757d93db6f53a29ef52140bd20
Robert Blackwell <rblackwell@flatironinstitute.org>
2024-09-12 10:32:15 -0400

    performance: factor out expensive mmap in calc_distances

--- ManifoldEM/calc_distance.py
+++ ManifoldEM/calc_distance.py
@@ -211,6 +211,8 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
     else:
         mask = annular_mask(0, n_pix / 2., n_pix, n_pix)
 
+    if relion_data:
+        img_data = mrcfile.mmap(img_file_name, 'r').data
 
     # read images with conjugates
     for i_part in range(n_particles):
@@ -222,9 +224,8 @@ def get_distance_CTF_local(input_data: LocalInput, filter_params: FilterParams, 
             start = n_pix**2 * raw_particle_index * 4
             img = np.memmap(img_file_name, dtype='float32', offset=start, mode='r', shape=(n_pix, n_pix)).T
         else:  # relion data
-            img = mrcfile.mmap(img_file_name, 'r').data[raw_particle_index]
             shi = (image_offsets[1][raw_particle_index] - 0.5, image_offsets[0][raw_particle_index] - 0.5)
-            img = shift(img, shi, order=3, mode='wrap')
+            img = shift(img_data[raw_particle_index], shi, order=3, mode='wrap')
         if indices[i_part] >= n_particles_tot / 2:  # second half data set
             img = np.flipud(img)
 
