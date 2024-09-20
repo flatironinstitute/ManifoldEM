44d84ade5f18146e7a461d091e4e376185ccc54b
Robert Blackwell <rblackwell@flatironinstitute.org>
2024-09-20 14:03:55 -0400

    data_store: add basic API to get information about a given prd
    from ManifoldEM.params import params
    from ManifoldEM.data import data_store
    
    params.load('params_myproject.toml')
    prd = data_store.get_prd_data(0)
    print(prd)
    
    ...
    
    prd_index: 0
    S2_bin_index: 14987
    bin_center: [0.14516903 0.05351538 0.98795853]
    n_images: 100
    defocus: 12114.310059
    occupancy: 100
    trash: False
    cluster_id: 0

--- ManifoldEM/data_store.py
+++ ManifoldEM/data_store.py
@@ -4,6 +4,7 @@ from enum import Enum
 import mrcfile
 import numpy as np
 import pickle
+from dataclasses import dataclass
 
 from typing import List, Any, Tuple, Dict, Set
 from nptyping import NDArray, Shape, Int, Int64, Float64, Bool
@@ -13,45 +14,6 @@ from ManifoldEM.star import get_align_data
 from ManifoldEM.quaternion import collapse_to_half_space, quaternion_to_S2
 from ManifoldEM.S2tessellation import bin_and_threshold
 from ManifoldEM.FindCCGraph import op as FindCCGraph
-
-
-class PrdData:
-    def __init__(self, prd_index: int):
-        prds = data_store.get_prds()
-        if prd_index >= prds.n_bins:
-            raise ValueError("Invalid prd index")
-
-        self.prd_index = prd_index
-        self.image_indices = prds.thresholded_image_indices[prd_index]
-        self._dist_data = None
-        self._raw_images = None
-
-        dist_file = params.get_dist_file(prd_index)
-        if os.path.isfile(dist_file):
-            with open(dist_file, 'rb') as f:
-                self._dist_data = pickle.load(f)
-
-        if self.image_indices.size:
-            img_stack_data = data_store.get_image_stack_data()
-            self._raw_images = np.empty(shape=(len(self.image_indices), params.ms_num_pixels, params.ms_num_pixels), dtype=np.float32)
-            for i, idx in enumerate(self.image_indices):
-                self._raw_images[i] = img_stack_data[idx]
-
-
-    @property
-    def raw_images(self):
-        return self._raw_images
-
-
-    @property
-    def transformed_images(self):
-        return self._dist_data['imgAll']
-
-
-    @property
-    def ctf_images(self):
-        return self._dist_data['CTF'].reshape(-1, params.ms_num_pixels, params.ms_num_pixels)
-
 
 class Sense(Enum):
     """
@@ -135,6 +97,135 @@ class Anchor:
         self.sense: Sense = sense
 
 
+@dataclass
+class PrdInfo:
+    """
+    Data class to store information about a projection direction.
+
+    Attributes
+    ----------
+    prd_index : int
+        The index of the projection direction after thresholding.
+    S2_bin_index : int
+        The index of the bin on the unit sphere. This is the index before thresholding.
+    bin_center : ndarray
+        The coordinates of center of the bin on the unit sphere.
+    defocus : float
+        The defocus value for the projection direction.
+    occupancy : int
+        The number of images in the bin of this projection direction.
+    trash : bool
+        Whether the projection direction is marked as trash. Trash projection directions are not used in analysis.
+    cluster_id : int
+        The cluster ID. Projection directions in one cluster use optical flow to detect 'senses' for neighboring prds in the same cluster.
+    n_images : int
+        The number of images in the bin of this projection direction.
+    raw_image_indices : ndarray
+        The `n_image` indices of the raw images in the image stack file.
+    image_centers : ndarray
+        The `n_image` centers of the images on the unit sphere.
+    image_quats : ndarray
+        The `n_image` quaternions of the images representing rotations from the z-axis to their position and rotation on the unit sphere.
+    """
+    prd_index: int
+    S2_bin_index: int
+    bin_center: NDArray[Shape["3"], Float64]
+    defocus: float
+    occupancy: int
+    trash: bool
+    cluster_id: int
+    n_images: int
+    raw_image_indices: NDArray[Shape["Any"], Int]
+    image_centers: NDArray[Shape["Any,3"], Int]
+    image_quats: NDArray[Shape["Any,4"], Float64]
+
+    def __repr__(self):
+        relevant_fields = ['prd_index', 'S2_bin_index', 'bin_center', 'n_images', 'defocus', 'occupancy', 'trash', 'cluster_id']
+        info_str = '\n'.join([f'{field}: {getattr(self, field)}' for field in relevant_fields])
+
+        return info_str
+
+
+class PrdData:
+    """
+    Represents a single projection direction, providing access to its raw and transformed images, CTF images, and metadata.
+
+    Attributes
+    ----------
+    info : PrdInfo
+        Metadata about the projection direction.
+    raw_images : ndarray
+        The raw images from the image stack associated with the projection direction.
+    transformed_images : ndarray
+        The filtered and "in-plane" rotated images associated with the projection direction.
+    ctf_images : ndarray
+        The Contrast Transfer Function (CTF) images associated with the projection direction.
+    """
+    def __init__(self, prd_index: int):
+        prds = data_store.get_prds()
+        if prd_index >= prds.n_bins:
+            raise ValueError("Invalid prd index")
+
+        self._image_indices = prds.thresholded_image_indices[prd_index]
+        self._dist_data = None
+        self._raw_images = None
+        self._info = PrdInfo(prd_index=prd_index,
+                             S2_bin_index=prds.thres_ids[prd_index],
+                             bin_center=prds.bin_centers[:, prds.thres_ids[prd_index]],
+                             defocus=prds.defocus[prd_index],
+                             occupancy=prds.occupancy[prd_index],
+                             trash=prd_index in prds.trash_ids,
+                             cluster_id=prds.cluster_ids[prd_index],
+                             n_images=len(self._image_indices),
+                             raw_image_indices=self._image_indices,
+                             image_centers=prds.pos_full[:, self._image_indices].T,
+                             image_quats=prds.quats_full[:, self._image_indices].T)
+
+
+    def _load_dist_data(self):
+        if self._dist_data is None:
+            dist_file = params.get_dist_file(self._info.prd_index)
+            if os.path.isfile(dist_file):
+                with open(dist_file, 'rb') as f:
+                    self._dist_data = pickle.load(f)
+            else:
+                msg = f"Distance data file not found: {dist_file}"
+                raise FileNotFoundError(msg)
+
+        return self._dist_data
+
+
+    @property
+    def info(self):
+        return self._info
+
+
+    @property
+    def raw_images(self):
+        if self._raw_images is None:
+            img_stack_data = data_store.get_image_stack_data()
+            self._raw_images = np.empty(shape=(len(self._image_indices), params.ms_num_pixels, params.ms_num_pixels), dtype=np.float32)
+            for i, idx in enumerate(self._image_indices):
+                self._raw_images[i] = img_stack_data[idx]
+
+        return self._raw_images
+
+
+    @property
+    def transformed_images(self):
+        return self._load_dist_data()['imgAll']
+
+
+    @property
+    def ctf_images(self):
+        self._load_dist_data()
+        return self._load_dist_data()['CTF'].reshape(-1, params.ms_num_pixels, params.ms_num_pixels)
+
+
+    def __repr__(self) -> str:
+        return self._info.__repr__()
+
+
 class _ProjectionDirections:
     """
     Manages and processes projection direction data, including thresholds, bin centers, defocus values, and more.
@@ -166,20 +257,20 @@ class _ProjectionDirections:
     def __init__(self):
         self.thres_low: int = params.prd_thres_low
         self.thres_high: int = params.prd_thres_high
-        self.bin_centers: NDArray[Shape["3,*"], Float64] = np.empty(shape=(3, 0))
+        self.bin_centers: NDArray[Shape["3,Any"], Float64] = np.empty(shape=(3, 0))
 
-        self.defocus: NDArray[Shape["*"], Float64] = np.empty(0)
-        self.microscope_origin: Tuple[NDArray[Shape["*"], Float64],
-                                      NDArray[Shape["*"], Float64]] = (np.empty(0), np.empty(0))
+        self.defocus: NDArray[Shape["Any"], Float64] = np.empty(0)
+        self.microscope_origin: Tuple[NDArray[Shape["Any"], Float64],
+                                      NDArray[Shape["Any"], Float64]] = (np.empty(0), np.empty(0))
 
-        self.pos_raw: NDArray[Shape["3,*"], Float64] = np.empty(shape=(3,0))
-        self.pos_full: NDArray[Shape["3,*"], Float64] = np.empty(shape=(3,0))
-        self.quats_raw: NDArray[Shape["4,*"], Float64] = np.empty(shape=(4,0))
-        self.quats_full: NDArray[Shape["4,*"], Float64] = np.empty(shape=(4,0))
+        self.pos_raw: NDArray[Shape["3,Any"], Float64] = np.empty(shape=(3,0))
+        self.pos_full: NDArray[Shape["3,Any"], Float64] = np.empty(shape=(3,0))
+        self.quats_raw: NDArray[Shape["4,Any"], Float64] = np.empty(shape=(4,0))
+        self.quats_full: NDArray[Shape["4,Any"], Float64] = np.empty(shape=(4,0))
 
-        self.image_indices_full: NDArray[Shape["*"], Any] = np.empty(0, dtype=object)
+        self.image_indices_full: NDArray[Shape["Any"], Any] = np.empty(0, dtype=object)
         self.thres_ids: list[int] = []
-        self.occupancy_full: NDArray[Shape["*"], Int] = np.empty(0, dtype=int)
+        self.occupancy_full: NDArray[Shape["Any"], Int] = np.empty(0, dtype=int)
 
         self.anchors: Dict[int, Anchor] = {}
         self.trash_ids: Set[int] = set()
@@ -191,12 +282,12 @@ class _ProjectionDirections:
         self.neighbor_graph_pruned: Dict[str, Any] = {}
         self.neighbor_subgraph_pruned: List[Dict[str, Any]] = []
 
-        self.pos_thresholded: NDArray[Shape["3,*"], Float64] = np.empty(shape=(3,0))
-        self.theta_thresholded: NDArray[Shape["*"], Float64] = np.empty(0)
-        self.phi_thresholded: NDArray[Shape["*"], Float64] = np.empty(0)
-        self.cluster_ids: NDArray[Shape["*"], Int] = np.empty(0, dtype=int)
+        self.pos_thresholded: NDArray[Shape["3,Any"], Float64] = np.empty(shape=(3,0))
+        self.theta_thresholded: NDArray[Shape["Any"], Float64] = np.empty(0)
+        self.phi_thresholded: NDArray[Shape["Any"], Float64] = np.empty(0)
+        self.cluster_ids: NDArray[Shape["Any"], Int] = np.empty(0, dtype=int)
 
-        self.image_is_mirrored: NDArray[Shape["*"], Bool] = np.empty(0, dtype=bool)
+        self.image_is_mirrored: NDArray[Shape["Any"], Bool] = np.empty(0, dtype=bool)
 
 
     def load(self, pd_file=None):
@@ -319,6 +410,30 @@ class _ProjectionDirections:
             self.anchors.pop(id)
 
 
+    def get_prd_data(self, id: int):
+        """
+        Returns a PrdData object for the given projection direction ID.
+
+        Parameters
+        ----------
+        id : int
+            The index of the projection direction.
+
+        Raises
+        ------
+        ValueError
+            If the ID is invalid.
+        """
+        if not isinstance(id, int):
+            raise TypeError("Invalid prd index type")
+
+        if id < 0 or id >= self.n_bins:
+            msg = f'Invalid prd index: {id}. Valid indices on [0, {self.n_bins})'
+            raise ValueError(msg)
+
+        return PrdData(id)
+
+
     @property
     def anchor_ids(self):
         """
@@ -422,6 +537,11 @@ class _DataStore:
         """
         self._projection_directions.update()
         return self._projection_directions
+
+
+    def get_prd_data(self, prd_index: int):
+        self._projection_directions.update()
+        return self._projection_directions.get_prd_data(prd_index)
 
 
     def get_image_stack_data(self):
