1618dd203db03f462395f63fed794a484bb659a1
Geoffrey Woollard <geoffwoollard@gmail.com>
2025-02-03 19:42:56 -0500

    test_quaternion_to_S2

--- tests/test_quaternion.py
+++ tests/test_quaternion.py
@@ -1,9 +1,10 @@
 import numpy as np
-
-from ManifoldEM.util import eul_to_quat
 from scipy.spatial.transform import Rotation
 
-def test_manifold_em_raw_quaternion():
+from ManifoldEM.util import eul_to_quat
+from ManifoldEM.quaternion import quaternion_to_S2
+
+def test_eul_to_quat():
     ''' ManifoldEM raw quaternion convention.
     
     The convention is different from the one used in scipy.spatial.transform.Rotation.
@@ -12,8 +13,8 @@ def test_manifold_em_raw_quaternion():
     n_euler_anlges = 3
     n_ransom_samples = 7
     atol = 1e-4
-    np.random.seed(0)
 
+    np.random.seed(0)
     euler_angles = np.random.uniform(low=-np.pi, high=np.pi, size=(n_ransom_samples, n_euler_anlges))
 
     phi, theta, psi, = euler_angles.T
@@ -45,6 +46,28 @@ def test_manifold_em_raw_quaternion():
     q_YXY = Rotation.from_euler(intrinsic_xzx, euler_angles, degrees=False).as_quat()
     permuted_YXY = np.vstack([-q2,-q3,-q1,q0,]).T
     np.isclose(q_YXY,permuted_YXY,atol=atol)
+
+
+def test_quaternion_to_S2():
+    n_euler_anlges = 3
+    n_ransom_samples = 7
+    atol = 1e-4
+
+    np.random.seed(0)
+    euler_angles = np.random.uniform(low=-np.pi, high=np.pi, size=(n_ransom_samples, n_euler_anlges))
+
+    rotation = Rotation.from_euler('ZXZ', euler_angles, degrees=False).as_matrix()
+    rxz, ryz, rzz = rotation[:,:,-1].T
+    s2 = np.stack([-ryz,rxz,rzz], axis=1)
+
+    phi, theta, psi, = euler_angles.T
+    flip = True
+    raw_qs = eul_to_quat(phi, theta, psi, flip=flip)
+    s2_mem = quaternion_to_S2(raw_qs).T
+
+    assert np.allclose(s2, s2_mem, atol=atol)
+
+
 
 
 
